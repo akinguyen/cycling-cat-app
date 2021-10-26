ec79abcf3e7e287e70c3fca3baffe4d08699956c
minhlongds0312 <minhlongduong1234@gmail.com>
2021-10-26 11:23:45 +0700

    xin loi mn em di an trua phat a, nha keu roi:(

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+﻿import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={{ uri: 'https://scontent.fsgn8-2.fna.fbcdn.net/v/t1.15752-9/245424195_563237511630518_8274433361488201924_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=ae9488&_nc_ohc=Mcc9cmEWSa4AX9S3hi4&_nc_ht=scontent.fsgn8-2.fna&oh=b813fa4dca789f6b432a6b6e9969802e&oe=619CFF92' }} style={styles.logo} />
+      <Text style={styles.instructions}>
+        Sức mạnh càng lớn, trách nhiệm càng cao!
+      </Text>
+
+      <TouchableOpacity
+        onPress={() => alert('Bạn đã đồng ý không ảo game')}
+        style={{ backgroundColor: 'blue' }}>
+        <Text style={{ fontSize: 20, color: '#fff' }}>
+        Bấm để đồng ý không bao giờ ảo game nữa
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,5 +26,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 300,
+    marginBottom: 20,
+  },
+  instructions: {
+    color: '#f72',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 10,
   },
 });
