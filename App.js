85181d23b863fd7ae50e1edc38d24a0618d1e436
Xi162 <huy.bui16bit@hcmut.edu.vn>
2021-10-26 11:02:51 +0700

    cai nay bi thieu nang

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={{ uri: 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t1.15752-9/245424195_563237511630518_8274433361488201924_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=ae9488&_nc_ohc=Mcc9cmEWSa4AX8GrPri&_nc_ht=scontent.fsgn5-5.fna&oh=2cb6bda6c17cebe22c31fa032924a445&oe=619CFF92' }} style={styles.logo} />
+      <Text style={{ fontSize: 20, color: '#fff' }}>
+        M có bị thiểu năng không!
+      </Text>
+
+      <TouchableOpacity
+        onPress={() => alert('m bị thiểu năng')}
+        style={{ backgroundColor: 'blue' }}>
+        <Text style={{ fontSize: 20, color: '#fff' }}>bấm vào để không bị thiểu năng</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +21,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#8B0000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 305,
+    marginBottom: 100,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 20,
+    marginBottom: 20,
   },
 });
