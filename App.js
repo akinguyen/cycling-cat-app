2228bcab20c4964aa9e2855502f52745ee4b6fc3
Duy Nguyen <duynguyenquykhanh@gmail.com>
2021-11-09 22:31:13 -0800

    update main

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>chỉ có duy thấy</Text>
-      <Text>chỉ có duy thấy</Text>
+    <View>
+      <Text>Hello Cat </Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
