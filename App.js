67ecbf4cb32e5d406fa734834fc2297d8ab22e8e
Duy Nguyen <36982208+akinguyen@users.noreply.github.com>
2021-11-09 22:50:11 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Text, View } from "react-native";
 
+// Trong video tutorial, mọi người thấy có dòng style={styles.container} nhưng code này thì ko có, vì vậy mọi người đừng lo lắng và đừng thêm nó vào phần <View> nhé,
+// Nhưng lát nữa anh có thêm cái <Login> với <SignUp> thì mấy bạn thêm được nè :D
+
 export default function App() {
   return (
     <View>
