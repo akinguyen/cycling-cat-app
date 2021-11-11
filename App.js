fb17b645f41083fa137d9dffbf19b4880abaf923
Xi162 <huy.bui16bit@hcmut.edu.vn>
2021-11-11 22:22:30 +0700

    first time slide 10

--- App.js
+++ App.js
@@ -1,65 +1,96 @@
-<<<<<<< HEAD
-﻿import React from 'react';
-=======
 import React from 'react';
->>>>>>> 79e3124603512c993984728fa63a885dc536bd12
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View, Input,Button, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-<<<<<<< HEAD
-      <Image source={{ uri: 'https://scontent.fsgn8-2.fna.fbcdn.net/v/t1.15752-9/245424195_563237511630518_8274433361488201924_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=ae9488&_nc_ohc=Mcc9cmEWSa4AX9S3hi4&_nc_ht=scontent.fsgn8-2.fna&oh=b813fa4dca789f6b432a6b6e9969802e&oe=619CFF92' }} style={styles.logo} />
-      <Text style={styles.instructions}>
-        Sức mạnh càng lớn, trách nhiệm càng cao!
-      </Text>
+    <SafeAreaView style = {styles.container}>
+      <View style = {styles.bar}>
+          <Text style = {{color: 'white'}} >MATCHED GROUPS</Text>
+        </View>
+        
+        <View style = {styles.groupinfo}>
+          
+          <Image source = { {uri: 'https://s.luyengame.net/games/pikachu/image.jpg'}} style = {styles.circle} />
 
-      <TouchableOpacity
-        onPress={() => alert('Bạn đã đồng ý đi ăn cơm')}
-        style={{ backgroundColor: 'blue' }}>
-        <Text style={{ fontSize: 20, color: '#fff' }}>
-        Bấm để đi ăn cơm
-        </Text>
-=======
-      <Image source={{ uri: 'https://i.imgur.com/TkIrScD.png' }} style={styles.logo} />
-      <Text style={styles.instructions}>
-        To share a photo from your phone with a friend, just press the button below!
-      </Text>
+          <View style = {styles.infoname}>
+          <Text>User name:       </Text>
+          <View style = {styles.infobox}>
+            <Text>- Member:     </Text>
+            <Text>- Leader:     </Text>
+            <Text>- Sports:     </Text>
+          </View>
+        </View>  
+      </View>
 
-      <TouchableOpacity
-        onPress={() => alert('Hello, world!')}
-        style={{ backgroundColor: 'green' }}>
-        <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>
->>>>>>> 79e3124603512c993984728fa63a885dc536bd12
-      </TouchableOpacity>
-    </View>
-  );
-}
+      <View style = {styles.groupinfo}>
+          
+          <Image source = { {uri: 'https://img.pokemondb.net/artwork/large/charizard-mega-x.jpg'}} style = {styles.circle} />
 
+          <View style = {styles.infoname}>
+          <Text>User name:       </Text>
+          <View style = {styles.infobox}>
+            <Text>- Member:     </Text>
+            <Text>- Leader:     </Text>
+            <Text>- Sports:     </Text>
+          </View>
+        </View>  
+      </View>
+
+      <View style = {styles.groupinfo}>
+          
+          <Image source = { {uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSntjrvQhcLbcd1R49jvD9ME7V3xhGxBDvdOA&usqp=CAU'}} style = {styles.circle} />
+
+          <View style = {styles.infoname}>
+          <Text>User name:       </Text>
+          <View style = {styles.infobox}>
+            <Text>- Member:     </Text>
+            <Text>- Leader:     </Text>
+            <Text>- Sports:     </Text>
+          </View>
+        </View>  
+      </View>
+    </SafeAreaView> 
+  )
+} 
+  
+  
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#8B0000',
-    alignItems: 'center',
+    alignItems:'center',
+
+  },
+  bar:{
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    marginLeft: 100,
+    marginRight:100,
+    marginTop: 80,
+    height: 40,
+    width: 250,
   },
-  logo: {
-    width: 305,
-<<<<<<< HEAD
-    height: 300,
-    marginBottom: 20,
+  circle:{
+    height: 120,
+    width: 120,  
+    
   },
-  instructions: {
-    color: '#f72',
-=======
-    height: 159,
-    marginBottom: 20,
+  groupinfo:{
+    marginTop: 75,
+    justifyContent: 'flex-start',
+    alignItems:'center',
+    flexDirection: 'row',
   },
-  instructions: {
-    color: '#888',
->>>>>>> 79e3124603512c993984728fa63a885dc536bd12
-    fontSize: 18,
-    marginHorizontal: 15,
-    marginBottom: 10,
+  infobox:{
+    height: 100,
+    width: 200,
+    borderTopRightRadius: 100,
+    borderBottomRightRadius: 100,
+    backgroundColor: '#CCFF99',
+    justifyContent: 'space-around'
   },
-});
+  infoname:{
+    alignItems: 'flex-start',
+    marginLeft: 2,
+  }
+})
