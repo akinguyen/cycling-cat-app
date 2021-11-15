6cac6fbdf0c22392f7e8693a38e50d1743cf211b
Xi162 <huy.bui16bit@hcmut.edu.vn>
2021-11-15 11:53:28 +0700

    add styles and reformat some files

--- App.js
+++ App.js
@@ -2,9 +2,6 @@ import React from "react";
 import { Text, View, Image, SafeAreaView, StyleSheet } from "react-native";
 import MatchGroup from "./src/MatchGroup";
 
-
 export default function App() {
-  return (
-    <MatchGroup></MatchGroup>
-  );
+  return <MatchGroup></MatchGroup>;
 }
