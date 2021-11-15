bc790880d66ea6705fa5e5a344d05898c4b5c777
Xi162 <huy.bui16bit@hcmut.edu.vn>
2021-11-15 09:01:19 +0700

    New  List of matchings

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, SafeAreaView, StyleSheet } from "react-native";
+import Listofmatchinggroups from "./src/list of matching groups";
 
-// Trong video tutorial, mọi người thấy có dòng style={styles.container} nhưng code này thì ko có, vì vậy mọi người đừng lo lắng và đừng thêm nó vào phần <View> nhé,
-// Nhưng lát nữa anh có thêm cái <Login> với <SignUp> thì mấy bạn thêm được nè :D
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello Cat </Text>
-    </View>
+    <Listofmatchinggroups></Listofmatchinggroups>
   );
 }
---
+++ src/list of matching groups/index.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { Text, View, Image, StyleSheet, SafeAreaView } from "react-native";
+
+export default function Listofmatchinggroups() {
+  return ( 
+    <SafeAreaView style = {styles.container}>
+      <View style = {styles.bar}>
+          <Text style = {{color: 'white'}} >MATCHED GROUPS</Text>
+        </View>
+        
+        <View style = {styles.groupinfo}>
+          
+          <Image source = { {uri: 'https://s.luyengame.net/games/pikachu/image.jpg'}} style = {styles.circle} />
+
+          <View style = {styles.infoname}>
+          <Text>User name:       </Text>
+          <View style = {styles.infobox}>
+            <Text>- Member:     </Text>
+            <Text>- Leader:     </Text>
+            <Text>- Sports:     </Text>
+          </View>
+        </View>  
+      </View>
+
+      <View style = {styles.groupinfo}>
+          
+          <Image source = { {uri: 'https://img.pokemondb.net/artwork/large/charizard-mega-x.jpg'}} style = {styles.circle} />
+
+          <View style = {styles.infoname}>
+          <Text>User name:       </Text>
+          <View style = {styles.infobox}>
+            <Text>- Member:     </Text>
+            <Text>- Leader:     </Text>
+            <Text>- Sports:     </Text>
+          </View>
+        </View>  
+      </View>
+
+      <View style = {styles.groupinfo}>
+          
+          <Image source = { {uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSntjrvQhcLbcd1R49jvD9ME7V3xhGxBDvdOA&usqp=CAU'}} style = {styles.circle} />
+
+          <View style = {styles.infoname}>
+          <Text>User name:       </Text>
+          <View style = {styles.infobox}>
+            <Text>- Member:     </Text>
+            <Text>- Leader:     </Text>
+            <Text>- Sports:     </Text>
+          </View>
+        </View>  
+      </View>
+    </SafeAreaView> 
+        
+    )
+    } 
+  
+  
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    alignItems:'center',
+
+  },
+  bar:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    marginLeft: 100,
+    marginRight:100,
+    marginTop: 80,
+    height: 40,
+    width: 250,
+  },
+  circle:{
+    height: 120,
+    width: 120,  
+    
+  },
+  groupinfo:{
+    marginTop: 75,
+    justifyContent: 'flex-start',
+    alignItems:'center',
+    flexDirection: 'row',
+  },
+  infobox:{
+    height: 100,
+    width: 200,
+    borderTopRightRadius: 100,
+    borderBottomRightRadius: 100,
+    backgroundColor: '#CCFF99',
+    justifyContent: 'space-around'
+  },
+  infoname:{
+    alignItems: 'flex-start',
+    marginLeft: 2,
+  }
+})
