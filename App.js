1de63bb53ad8aae3039379c9ac5d745f8f7cd7fa
Lolicat2103 <tien.nguyen2103@hcmut.edu.vn>
2021-11-15 15:56:36 +0700

    Slide 4

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-// Trong video tutorial, mọi người thấy có dòng style={styles.container} nhưng code này thì ko có, vì vậy mọi người đừng lo lắng và đừng thêm nó vào phần <View> nhé,
-// Nhưng lát nữa anh có thêm cái <Login> với <SignUp> thì mấy bạn thêm được nè :D
+
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello Cat </Text>
+    
+    <View style={{height: "100%", width: "100%",justifyContent: 'center', alignItems: 'center'}}>
+      <View style={{height: "25%", }}>
+      <Text style={{color: "white", textAlign: "centre", backgroundColor: "#5DC82F", padding: 8, fontSize: 19}}> Which sport do you prefer? </Text>
+      </View>
+      <View style={{flexDirection: "row", width: "80%", justifyContent: 'space-around'}}>
+          <View style={{backgroundColor: '#5DC82F', width: 110, height: 50, borderRadius: 200, marginBottom: 20, justifyContent: 'center', alignItems: 'center'}}>
+              <Text style={{color: "white", textAlign: "centre", fontSize:17}}> Running </Text>
+          </View>
+          <View style={{backgroundColor: '#5DC82F', width: 110, height: 50, borderRadius: 200, marginBottom: 20,justifyContent: 'center', alignItems: 'center'}}>
+              <Text style={{color: "white", textAlign: "centre", fontSize:17}}> Gym </Text>
+          </View>
+      </View>
+      
+      <View style={{flexDirection: "row", width: "80%", justifyContent: 'space-around'}}>
+          <View style={{backgroundColor: '#7ed957', width: 110, height: 50, borderRadius: 200, marginBottom: 20, justifyContent: 'center', alignItems: 'center'}}>
+              <Text style={{color: "white", textAlign: "centre", fontSize:17}}> Cycling </Text>
+          </View>
+          <View style={{backgroundColor: '#7ed957', width: 110, height: 50, borderRadius: 200, marginBottom: 20,justifyContent: 'center', alignItems: 'center'}}>
+              <Text style={{color: "white", textAlign: "centre", fontSize:17}}> Yoga </Text>
+          </View>
+      </View>
+
+      <View style={{flexDirection: "row", width: "80%", justifyContent: 'space-around'}}>
+          <View style={{backgroundColor: '#8FEB68', width: 110, height: 50, borderRadius: 200, marginBottom: 20, justifyContent: 'center', alignItems: 'center'}}>
+              <Text style={{color: "white", textAlign: "centre", fontSize:17}}> Badminton </Text>
+          </View>
+          <View style={{backgroundColor: '#8FEB68', width: 110, height: 50, borderRadius: 200, marginBottom: 20,justifyContent: 'center', alignItems: 'center'}}>
+              <Text style={{color: "white", textAlign: "centre", fontSize:17}}> Aerobics </Text>
+          </View>
+      </View>
+    
+      
     </View>
+
+    
+    
+     
+   
   );
 }
