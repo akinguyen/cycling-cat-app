9ea12cc3141b7e0fcc5009c86b7f466c37d46910
grassnhi <nhi.nguyenn@hcmut.edu.vn>
2021-11-17 23:04:54 +0700

    Push to nhi

--- src/Calculator1/index.js
+++ src/Calculator1/index.js
@@ -1,48 +1,30 @@
 import * as React from "react";
 import { Text, View } from "react-native";
+import styles from "./styles";
 
-export default function Calculator1() {
+export default function Welcome() {
   return (
-    <View
-      style={{
-        backgroundColor: "white",
-        height: "100%",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "FADFF2",
-          height: "20%",
-          width: "100%",
-          alignItems: "center",
-          justifyContent: "centre",
-        }}
-      >
+    <View style={styles.Main}>
+      <View style={styles.Rect}>
         <Text style={{ fontSize: 20, justifyContent: "centre" }}>
           {" "}
           Personal Training{" "}
         </Text>
       </View>
 
-      <View
-        style={{
-          backgroundColor: "white",
-          height: "100%",
-          flexDirection: "column",
-          alignItems: "center",
-          flexDirection: "space-around",
-        }}
-      >
-        <Text
-          style={{
-            backgroundColor: "ADFF2F",
-          }}
-        >
-          {" "}
-          Choose your goal!{" "}
-        </Text>
+      <View style={styles.White}>
+        <View style={styles.Choose}>
+          <Text style={styles.Text}> Choose your goal! </Text>
+        </View>
+        <View style={styles.Choose}>
+          <Text style={styles.Text}> Lose weight </Text>
+        </View>
+        <View style={styles.Choose}>
+          <Text style={styles.Text}> General workout </Text>
+        </View>
+        <View style={styles.Choose}>
+          <Text style={styles.Text}> Lose weight </Text>
+        </View>
       </View>
     </View>
   );
