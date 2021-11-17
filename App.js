86648d9fdc27441fb95519c05d8f73721cae4469
Xi162 <huy.bui16bit@hcmut.edu.vn>
2021-11-17 18:02:51 +0700

    add logo

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Text, View, Image, SafeAreaView, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  LogBox,
+} from "react-native";
 import GroupActivity from "./src/GroupActivitiy";
 import GroupDescription from "./src/GroupDescription";
 import GroupMember from "./src/GroupMember";
@@ -7,5 +14,26 @@ import MatchGroup from "./src/MatchGroup";
 import NotifyMatching from "./src/NotifyMatching";
 
 export default function App() {
-  return <GroupMember />;
+  return (
+    <View
+      style={{
+        backgroundColor: "white",
+        height: "100%",
+        flexDirection: "column-reverse",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text style={{ fontSize: 50, color: "#34A47F" }}> Welcome! </Text>
+
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Image source={require("./asset/logo.png")} />
+      </View>
+    </View>
+  );
 }
