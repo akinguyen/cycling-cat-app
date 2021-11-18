f205be68f3cbce449c22f92408f1718a83094a59
Duy Nguyen <duynguyenquykhanh@gmail.com>
2021-11-17 16:57:22 -0800

    update main

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-﻿import React from "react";
-import { Text, View } from "react-native";
+﻿import React, { useState } from "react";
+import { TouchableWithoutFeedback } from "react-native";
 import PickSport from "./src/PickSport";
+import GroupDescription from "./src/GroupDescription";
+import Login from "./src/Login";
+import MatchGroup from "./src/MatchGroup";
+import NotifyMatching from "./src/NotifyMatching";
+import Profile from "./src/Profile";
+import Welcome from "./src/Welcome";
+
+const screens = [
+  <Welcome />,
+  <Login />,
+  <MatchGroup />,
+  <PickSport />,
+  <Profile />,
+  <NotifyMatching />,
+  <GroupDescription />,
+];
 
 export default function App() {
+  const [order, setOrder] = useState(0);
+
   return (
-    <View>
-      <PickSport></PickSport>
-    </View>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        setOrder(order < screens.length - 1 ? order + 1 : 0);
+      }}
+    >
+      {screens[order]}
+    </TouchableWithoutFeedback>
   );
 }
