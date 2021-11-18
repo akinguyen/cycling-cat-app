20d3f3e702b7e0f5065ab5cf97340103f6e251e3
grassnhi <nhi.nguyenn@hcmut.edu.vn>
2021-11-18 19:33:51 +0700

    Pull from main

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Text, View } from "react-native";
 import Welcome from "./src/Welcome";
 import Option from "./src/Option";
-import Calculator1 from "./src/Calculator1";
+import Calculator1 from "./src/ChooseGoal";
 export default function App() {
   return (
     <View>
