de83cbe53ff7490ab1761bf30e2e31c5237e040f
Xi162 <huy.bui16bit@hcmut.edu.vn>
2021-11-26 16:39:31 +0700

    add eventlist

---
+++ src/EventList/index.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { Text, View, Image, SafeAreaView, ScrollView } from "react-native";
+import styles from "./styles";
+
+export default function EventList() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.bar}>
+        <Text style={{ color: "white", fontSize: 25 }}>DAILY EVENT</Text>
+      </View>
+      <ScrollView>
+        <View style={styles.groupinfo}>
+          <Image
+            source={{ uri: "https://s.luyengame.net/games/pikachu/image.jpg" }}
+            style={styles.circle}
+          />
+
+          <View style={styles.infoname}>
+            <Text style={{ fontWeight: "bold", fontSize: 20 }}>Event </Text>
+            <View style={styles.infobox}>
+              <Text style={{ fontSize: 20 }}>Desscription:...</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.groupinfo}>
+          <Image
+            source={{
+              uri: "https://img.pokemondb.net/artwork/large/charizard-mega-x.jpg",
+            }}
+            style={styles.circle}
+          />
+
+          <View style={styles.infoname}>
+            <Text style={{ fontWeight: "bold", fontSize: 20 }}>Event </Text>
+            <View style={styles.infobox}>
+              <Text style={{ fontSize: 20 }}>Desscription:...</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.groupinfo}>
+          <Image
+            source={{
+              uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSntjrvQhcLbcd1R49jvD9ME7V3xhGxBDvdOA&usqp=CAU",
+            }}
+            style={styles.circle}
+          />
+
+          <View style={styles.infoname}>
+            <Text style={{ fontWeight: "bold", fontSize: 20 }}>Event </Text>
+            <View style={styles.infobox}>
+              <Text style={{ fontSize: 20 }}>Desscription:...</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.groupinfo}>
+          <Image
+            source={{ uri: "https://s.luyengame.net/games/pikachu/image.jpg" }}
+            style={styles.circle}
+          />
+
+          <View style={styles.infoname}>
+            <Text style={{ fontWeight: "bold", fontSize: 20 }}>Event </Text>
+            <View style={styles.infobox}>
+              <Text style={{ fontSize: 20 }}>Desscription:...</Text>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
---
+++ src/EventList/styles.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  bar: {
+    alignItems: "center",
+    backgroundColor: "black",
+    marginLeft: 100,
+    marginRight: 100,
+    marginTop: 80,
+    height: 40,
+    width: 250,
+    justifyContent: "center",
+  },
+  circle: {
+    height: 127,
+    width: 127,
+  },
+  groupinfo: {
+    marginTop: 75,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  infobox: {
+    height: 100,
+    width: 200,
+    borderTopRightRadius: 100,
+    borderBottomRightRadius: 100,
+    backgroundColor: "#CCFF99",
+    padding: 10,
+  },
+  infoname: {
+    alignItems: "flex-start",
+    marginLeft: 2,
+  },
+});
+
+export default styles;
