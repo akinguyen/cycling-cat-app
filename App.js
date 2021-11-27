e54c7c1f184b6954c76b19268b477c482e35b2b6
grassnhi <nhi.nguyenn@hcmut.edu.vn>
2021-11-27 10:56:02 +0700

    update

--- App.js
+++ App.js
@@ -4,34 +4,11 @@ import Login from "./src/Login";
 //import Profile from "./src/Profile";
 import Welcome from "./src/Welcome";
 import Logo from "./src/Logo";
-<<<<<<< HEAD
-import GroupActivity from "./src/GroupActivitiy";
-//import GroupMember from "./src/GroupMember";
-import CheckParticipation from "./src/CheckParticipation";
-
-const screens = [
-  <Logo />,
-  <Login />,
-  <Option />,
-  <Welcome />,
-  <MatchGroup />,
-  <PickSport />,
-  <NotifyMatching />,
-  <GroupActivity />,
-  <GroupDescription />,
-  // <GroupMember />,
-  <ChooseGoal />,
-  <CalculatorOne />,
-  <CalculatorTwo />,
-  <CheckParticipation />,
-];
-=======
 import EventList from "./src/EventList";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const screens = [<Logo />, <Login />, <Welcome />, <EventList />];
->>>>>>> e416d427ee7021db152ddbd1d37747cb94357347
 
 export default function App() {
   const [order, setOrder] = useState(0);
